8ddc1e4a4e18f28801852cc81e42da10148c0129
jeffshen18 <jeffreyshen18@gmail.com>
2018-01-10 17:23:20 -0700

    testing routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+//BrowserRouter interacts with history library and URL, Route is a react component that provides config that deciedes what component to show based on URL
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+  render() { return <div> Hello! </div>}
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye!</div>}
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello}/>
+        <Route path="/goodbye" component={Goodbye}/>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
