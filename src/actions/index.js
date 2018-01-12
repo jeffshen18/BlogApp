396b0cd9d5c1a97451f88965891fb858ddedc7ab
jeffshen18 <jeffreyshen18@gmail.com>
2018-01-12 09:57:09 -0700

    added action for fetching posts

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=jeffreyshen18';
+
+export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+  return {
+    type: FETCH_POSTS,
+    //because the request is assigned to payload, the redux-promise middleware will give payload the list of posts after it retrieves it
+    payload: request
+  };
+}
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 //BrowserRouter interacts with history library and URL, Route is a react component that provides config that deciedes what component to show based on URL
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 // don't need App anymore because React router does the work
 // import App from './components/app';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
