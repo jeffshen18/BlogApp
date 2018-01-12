f899eb2ef9520775fb56cdd8fb25f8a0432e938d
jeffshen18 <jeffreyshen18@gmail.com>
2018-01-12 11:11:15 -0700

    added react life cycle method and testing fetchPosts retrieval

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+  //called one time automatically when component first renders
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>
-        Posts Index 
+        Posts Index
       </div>
     );
   }
 }
 
-export default PostsIndex;
+//Putting fetchPosts inside is the same as using mapDispatchToProps
+//Whenever fetchPosts is being called, the the result is still being dispatched to all of our reducers
+export default connect(null, { fetchPosts }) (PostsIndex);
