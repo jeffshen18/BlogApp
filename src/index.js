dbbcf14ce3a226d64a24dd085aa044afed7853e2
jeffshen18 <jeffreyshen18@gmail.com>
2018-01-13 15:23:04 -0700

    adding Switch react router component for correct routing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 //BrowserRouter interacts with history library and URL, Route is a react component that provides config that deciedes what component to show based on URL
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 // don't need App anymore because React router does the work
 // import App from './components/app';
@@ -17,8 +17,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
-        <Route path="/posts/new" component={PostsNew} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
