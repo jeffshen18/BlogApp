c61922357bfc443d11a10776fbec024b7cdb9db8
jeffshen18 <jeffreyshen18@gmail.com>
2018-01-14 10:55:49 -0700

    adding redux-form into post_new component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  //Have to wire up to Field, field object contains event handlers to JSX that we are returning
+  renderTitleField(field) {
+    return (
+      <div>
+        <input
+          //field.input contains things like onChange, onBlur...
+          //Fancy JSX that saves us from typing out onChange = {field.input.onChange} ...
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
+
   render() {
     return (
+      //Field component is responsible for event handling and updating peices of state
+      //component returns some amount of JSX that represents actually JSX rendered to screen
       <form>
         <Field
           name="title"
+          //component prop calls function that returns some amount of JSX
+          //we do not call function here, but a reference to the function
+          component={this.renderTitleField}
         />
       </form>
     );
